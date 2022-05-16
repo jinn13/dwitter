583763e187610194ce403a6118b6915454c36ec0
jinn13 <dydn512@gmail.com>
2022-05-16 23:57:32 +0900

    220516

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,29 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  // 첫번째 값 : 관리되고 있는 값
+  // 두번째 값 :추후 새로운 title을 설정하기 위해 호출할 수 있는 함수
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!!");
+    // title = 'Updated!!';
+
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
