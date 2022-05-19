55233b45deaf9eaf654eb63e903d935563fbb0d8
jinn13 <dydn512@gmail.com>
2022-05-19 13:58:34 +0900

    20220519

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
@@ -7,24 +7,25 @@ import Card from "../UI/Card";
 const ExpenseItem = (props) => {
   // 첫번째 값 : 관리되고 있는 값
   // 두번째 값 :추후 새로운 title을 설정하기 위해 호출할 수 있는 함수
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle("Updated!!");
-    // title = 'Updated!!';
+  // const clickHandler = () => {
+  //   setTitle("Updated!!");
+  //   // title = 'Updated!!';
 
-    console.log(title);
-  };
+  //   console.log(title);
+  // };
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   //                                ↓ 매개변수 값은 자유롭게 지정 가능
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
@@ -11,12 +12,29 @@ const NewExpense = (props) => {
     };
     // console.log(expenseData);
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>데이터 추가하기</button>
+      )}
       {/* ↓ onSaveExpenseData의 경우 이름 자유롭게 지정 가능함 */}
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
