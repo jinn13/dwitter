406f22202a337486662935deb867a528aa1d5e7d
jinn13 <dydn512@gmail.com>
2022-07-02 03:14:14 +0900

    20220702

--- 10-Reducer/src/components/MainHeader/MainHeader.js
+++ 10-Reducer/src/components/MainHeader/MainHeader.js
@@ -7,7 +7,7 @@ const MainHeader = (props) => {
   return (
     <header className={classes["main-header"]}>
       <h1>메인페이지 입니다.</h1>
-      <Navigation onLogout={props.onLogout} />
+      <Navigation />
     </header>
   );
 };
