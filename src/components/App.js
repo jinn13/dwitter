fd1c2ab40adda362a056d1f307fce977c6e80a98
jinn13 <dydn512@gmail.com>
2022-07-22 23:54:17 +0900

    로그인 에러 해결중

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedin, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedin, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  // setInterval(() => console.log(authService.currentUser), 2000);
   // console.log(authService.currentUser);
   return (
     <>
-      <AppRouter isLoggedin={isLoggedin} />
+      {init ? <AppRouter isLoggedin={isLoggedin} /> : "initializing..."}
       <footer>&copy; {new Date().getFullYear()} Dwitter</footer>
     </>
   );
