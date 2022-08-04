a51da77846753b61c4073c6a7ed10e4cfcb93051
jinn13 <dydn512@gmail.com>
2022-08-04 23:20:16 +0900

    20220804

---
+++ 11-Food_Order_App/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Header from "./component/Layout/Header";
+
+function App() {
+  return (
+    <Fragment>
+      {/* <h2>Let's get started!</h2> */}
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
